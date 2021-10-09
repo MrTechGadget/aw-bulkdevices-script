931732a474734773449e84259697a8a8181d14f7
Joshua Clark <josh@mrtechgadget.com>
2021-10-08 20:55:32 -0400

    Added support for Serial as well as Device ID (#19)

--- Query-Device.ps1
+++ Query-Device.ps1
@@ -9,21 +9,26 @@
 .OUTPUTS
   NO OUTPUT CURRENTLY:Outputs a CSV log of actions
 .NOTES
-  Version:        1.0
+  Version:        1.1
   Author:         Joshua Clark @MrTechGadget
   Creation Date:  4/21/2021
+  Update Date:    10/08/2021
   Site:           https://github.com/MrTechGadget/aw-bulkdevices-script
 .EXAMPLE
   Query-Device.ps1 -file .\devices.csv -fileColumn "device_id"
+  Query-Device.ps1 -file .\serial.csv -fileColumn "SerialNumber"
 #>
 
 [CmdletBinding()] 
 Param(
-   [Parameter(Mandatory=$True,Position=0,ValueFromPipeline=$true,HelpMessage="Path to file listing Serial Numbers.")]
-   [string]$file,
+  [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $true, 
+    HelpMessage = "Path to file listing Device IDs or Serial Numbers.")]
+  [string]$file,
 	
-   [Parameter(HelpMessage="Name of Device ID column in file, default is device_id")]
-   [string]$fileColumn = "device_id"
+  [Parameter(HelpMessage = "Name of Device Identifier column in file, default is device_id, 
+    if using a serial number, as long as serial is part of the column it will query 
+    based on a serial number instead of device ID.")]
+  [string]$fileColumn = "device_id"
 )
 
 Import-Module .\PSairwatch.psm1
@@ -33,18 +38,34 @@ Start of Script
 #>
 
 $devicelist = Read-File $file $fileColumn
-$i=0
-foreach ($device in $devicelist) {
+$i = 0
+if ($fileColumn -like "*serial*" ) {
+
+  foreach ($device in $devicelist) {
     $i++
     #Write-Progress -Activity "Querying Devices" -Status "$($i) of $($devicelist.Count)" -PercentComplete ((($i)/(@($devicelist).Count))*100)
     Write-Host "$i of $($deviceList.Count)"
     $json = ' '
-    $endpoint = "mdm/devices/$device/commands?command=DeviceQuery" 
+    $endpoint = "mdm/devices/commands?command=DeviceQuery&searchby=SerialNumber&id=$device"
     try {
-        $queryresult = Send-Post $endpoint $json
+      Send-Post $endpoint $json
     }
     catch {
-        Write-Host "Error sending query command"
+      Write-Host "Error sending query command"
     }
-    
+  }
+}
+else {
+  foreach ($device in $devicelist) {
+    $i++
+    Write-Host "$i of $($deviceList.Count)"
+    $json = ' '
+    $endpoint = "mdm/devices/$device/commands?command=DeviceQuery"
+    try {
+      Send-Post $endpoint $json
+    }
+    catch {
+      Write-Host "Error sending query command"
+    }
+  }
 }
