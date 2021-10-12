504d4a3625c4b0417cc5044d610ca1b6a8ccc37f
MrTechGadget <josh@mrtechgadget.com>
2021-10-12 16:18:39 -0400

    Added support for Serial Number for Install Apps

--- Install-Application.ps1
+++ Install-Application.ps1
@@ -9,9 +9,10 @@
 .OUTPUTS
   NO OUTPUT CURRENTLY:Outputs a CSV log of actions
 .NOTES
-  Version:        1.0
+  Version:        1.1
   Author:         Joshua Clark @MrTechGadget
   Creation Date:  4/20/2021
+  Update Date:    10/12/2021
   Site:           https://github.com/MrTechGadget/aw-bulkdevices-script
 .EXAMPLE
   Install-Application.ps1 -file .\Devices.csv -fileColumn "device_id" -appId "12345" -appType "purchased"
@@ -19,17 +20,14 @@
 
 [CmdletBinding()] 
 Param(
-   [Parameter(Mandatory=$True,Position=0,ValueFromPipeline=$true,HelpMessage="Path to file listing Serial Numbers.")]
-   [string]$file,
-	
-   [Parameter(HelpMessage="Name of Serial Number column in file, default is SerialNumber")]
-   [string]$fileColumn = "SerialNumber",
-
-   [Parameter(Mandatory=$True,HelpMessage="ID of Application")]
-   [string]$appId,
-
-   [Parameter(Mandatory=$True,HelpMessage="App Type: internal, public, or purchased")]
-   [string]$appType
+  [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $true, HelpMessage = "Path to file listing Serial Numbers.")]
+  [string]$file,
+  [Parameter(HelpMessage = "Name of Serial Number column in file, default is SerialNumber")]
+  [string]$fileColumn = "SerialNumber",
+  [Parameter(Mandatory = $True, HelpMessage = "ID of Application")]
+  [string]$appId,
+  [Parameter(Mandatory = $True, HelpMessage = "App Type: internal, public, or purchased")]
+  [string]$appType
 )
 
 Import-Module .\PSairwatch.psm1
@@ -37,6 +35,7 @@ Import-Module .\PSairwatch.psm1
 <#
 Start of Script
 #>
+Write-Log "$($MyInvocation.Line)"
 
 $devicelist = Read-File $file $fileColumn
 #$OrgGroups = Get-OrgGroups
@@ -45,17 +44,21 @@ $devicelist = Read-File $file $fileColumn
 
 #$ApplicationList = Get-Applications $GroupID $Platform
 #$ApplicationSelected = Select-Tag $ApplicationList
-$i=0
+$i = 0
 foreach ($device in $devicelist) {
-    $i++
-    #Write-Progress -Activity "Installing Application" -Status "$($i) of $($devicelist.Count)" -CurrentOperation "$($device.$fileColumn)" -PercentComplete ((($i)/(@($devicelist).Count))*100)
-    Write-Host "$i of $($deviceList.Count)"
-    $json = '{ "DeviceId": '+ $device +' }'
-    try {
-        $installresults = Install-App $json $appId $appType
-    }
-    catch {
-        Write-Warning "Error Installing App"
-    }
-    
+  $i++
+  #Write-Progress -Activity "Installing Application" -Status "$($i) of $($devicelist.Count)" -CurrentOperation "$($device.$fileColumn)" -PercentComplete ((($i)/(@($devicelist).Count))*100)
+  Write-Host "$i of $($deviceList.Count)"
+  if ($fileColumn -like "*serial*" ) {
+    $json = '{ "SerialNumber": ' + $device + ' }'
+  }
+  else {
+    $json = '{ "DeviceId": ' + $device + ' }'
+  }
+  try {
+    $installresults = Install-App $json $appId $appType
+  }
+  catch {
+    Write-Warning "Error Installing App $device"
+  }
 }
